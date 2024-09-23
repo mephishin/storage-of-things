46b9723cbb58794d99247b8a8e87503441c7846d
mephishin <epishin1anatoly@gmail.com>
2024-09-23 22:27:12 +0300

    удалил неиспользуемый импорт

--- src/main/java/com/example/storageofthings/domain/security/ThingUser.java
+++ src/main/java/com/example/storageofthings/domain/security/ThingUser.java
@@ -10,7 +10,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.text.MessageFormat;
 import java.util.Collection;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @Entity
 @Getter
